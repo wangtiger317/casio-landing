06da03aa6c9f3d741058756b19c3abf699a0106c
wangtiger317 <hkc317@outlook.com>
2025-08-25 22:35:58 +0800

    updates data sharing with context

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { setContext } from 'svelte';
 	let { children, data } = $props();
+	setContext('data', data);
 </script>
 
 <svelte:head>
